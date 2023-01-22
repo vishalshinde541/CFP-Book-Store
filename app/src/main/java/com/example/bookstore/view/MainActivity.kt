1c80f30178f8d7f4fc0f1968b711136bda89cdfb
vishalshinde541 <vishalshinde541@gmail.com>
2023-01-22 13:05:06 +0530

    Added splash screen

--- app/src/main/java/com/example/bookstore/view/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.bookstore
+package com.example.bookstore.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookstore.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/bookstore/view/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bookstore.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.bookstore.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+       val handler = Handler()
+        handler.postDelayed({
+
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, (3000))
+    }
+}
